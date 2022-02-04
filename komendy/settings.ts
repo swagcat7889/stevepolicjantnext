3e14c6e1dca2af32934ea15148da165d95785e3c
burger <swagcat@swag.cat>
2022-02-04 18:48:20 +0000

    fix

--- komendy/settings.ts
+++ komendy/settings.ts
@@ -15,6 +15,18 @@ module.exports = {
                         global.serverConf.set(message.guild.id, 'editrola', args[2]);
                         return message.reply('Gotowe!');
                         break;
+                    case 'stivekrola':
+                        if (isNaN(parseInt(args[2], 10))) return message.reply('404 invalid rola');
+                        if (parseInt(args[2], 10) < message.guild.id) return message.reply('Na serio myślisz, że się na to nabiorę?');
+                        global.serverConf.set(message.guild.id, 'stivekrola', args[2]);
+                        return message.reply('Gotowe!');
+                        break;
+                    case 'izolatkarola':
+                        if (isNaN(parseInt(args[2], 10))) return message.reply('404 invalid rola');
+                        if (parseInt(args[2], 10) < message.guild.id) return message.reply('Na serio myślisz, że się na to nabiorę?');
+                        global.serverConf.set(message.guild.id, 'izolatkarola', args[2]);
+                        return message.reply('Gotowe!');
+                        break;
                     default:
                         return message.reply('Jak coś to możesz użyć adminrole...');
                 }
