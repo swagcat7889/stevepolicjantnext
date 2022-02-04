3b6d8848fec62717f3bf65eb4c934d207509395a
burger <swagcat@swag.cat>
2022-02-04 18:41:40 +0000

    ciekawe

--- komendy/izolatka.ts
+++ komendy/izolatka.ts
@@ -1,23 +1,19 @@
-const ms = require('ms');
-
-let sleep = (ms) => new Promise((a) => setTimeout(a, ms));
+const mparser = require('ms');
 
 module.exports = {
-    "name": "warn",
+    "name": "izolatka",
     async execute(message, args) {
         if (global.serverConf.get(message.guild.id, 'editrola') === null) throw new Error('Rola do edycji nie została zdefiniowana!');
         if (global.serverConf.get(message.guild.id, 'stivekrola') === null) throw new Error('Rola Stivek nie zdefiniowana!');
         if (global.serverConf.get(message.guild.id, 'izolatkarola') === null) throw new Error('Rola Izolatka nie zdefiniowana!');
-        if (!message.guild.members.cache.get(message.author.id).roles.cache.some((r) => r.id === global.serverConf.get(message.guild.id, 'editrola'))) return message.reply('Już Ciebie warnuję...');
+        if (!message.guild.members.cache.get(message.author.id).roles.cache.some((r) => r.id === global.serverConf.get(message.guild.id, 'editrola'))) return message.reply('Już Ciebie izoluję...');
 
         let victim = message.mentions.members.first();
 
-        if (!victim) {
-            return message.reply('Super, nie ma kogo zwarnować...');
-        }
+        if (!victim) return message.reply('To jest dosyć zaawansowany bot, ale nie aż tak zaawansowany, aby Ci czytać w myślach!');
 
         if (victim.bot) return message.reply('gigabruh');
         
-        global.warn(message, victim);
+        global.izolatka(message, victim, mparser(args[1]));
     }
 }
