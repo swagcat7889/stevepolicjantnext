40ddc6a78d4fad792c553a1d1ab880616b7ea4e2
burger <swagcat@swag.cat>
2022-02-08 16:06:33 +0000

    izolatka moment

--- komendy/izolatka.ts
+++ komendy/izolatka.ts
@@ -6,6 +6,7 @@ module.exports = {
         if (global.serverConf.get(message.guild.id, 'editrola') === null) throw new Error('Rola do edycji nie została zdefiniowana!');
         if (global.serverConf.get(message.guild.id, 'stivekrola') === null) throw new Error('Rola Stivek nie zdefiniowana!');
         if (global.serverConf.get(message.guild.id, 'izolatkarola') === null) throw new Error('Rola Izolatka nie zdefiniowana!');
+        if (global.serverConf.get(message.guild.id, 'kanalkary') === null) throw new Error('Kanał od kar nie został zdefiniowany!');
         if (!message.guild.members.cache.get(message.author.id).roles.cache.some((r) => r.id === global.serverConf.get(message.guild.id, 'editrola'))) return message.reply('Już Ciebie izoluję...');
 
         let victim = message.mentions.members.first();
