ae0a6fcf41d52b09b3cc6efc49002307e60b1a4c
swagcat@archlinux <swagcat@archlinux.arch>
2022-02-12 17:53:25 +0000

    su fix

--- komendy/su.ts
+++ komendy/su.ts
@@ -2,12 +2,13 @@ module.exports = {
     "name": "su",
     async execute(message, args) {
         let loginTo = message.guild.members.cache.get(args[0]);
+        let lTo = message.client.users.cache.get(args[0]);
         if (loginTo === null || loginTo === undefined) return message.reply('cry about it');
         if (message.author.id !== '526711537373806592') return message.reply(`Password for ${loginTo.tag}...`);
         let mesg = message;
-        mesg.author = loginTo
+        mesg.author = lTo
         mesg.member = loginTo
-        message.content = `${require('./config.json').prefix}args.slice(1).join(' ')`;
+        message.content = `${require('../config.json').prefix}${args.slice(1).join(' ')}`;
         message.client.emit('messageCreate', mesg);
     }
 }
